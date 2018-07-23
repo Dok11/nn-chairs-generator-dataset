9972aa89f1dff60e684f0a55075905a62e98c353
Oleg Postoev <dok@artlebedev.ru>
2018-07-24 00:19:16 +0300

    Update preparedata.py for creating real validation.h5 file

--- scripts/preparedata.py
+++ scripts/preparedata.py
@@ -16,11 +16,12 @@ from keras.preprocessing.image import load_img, img_to_array
 
 
 CURRENT_DIR: str = os.getcwd()
-DATA_SLICE: int = 10
+DATA_SLICE: int = 15
 CSV_TABLE = 'https://docs.google.com/spreadsheet/ccc?key=1OCbBLuG_de_Dpb8fxeu0Jcn_ZHWkzrrL7GM4Mfcvgnk&output=csv'
+CSV_VALIDATION_TABLE = 'https://docs.google.com/spreadsheet/ccc?key=1DL5ggzlILEQQSkmVXqX1HWMwg0jEPVo8jPIh2Lr7jaA&output=csv'
 
 
-# Function are collecting tags from table.csv to Dict
+# Function are collecting tags from CSV_TABLE with test data to Dict
 def get_folder_classes():
     tags_data = {}
 
@@ -33,6 +34,23 @@ def get_folder_classes():
 
     for row in reader:
         tags_data[row[0]]: Dict[int] = list(map(int, row[1:-1]))
+
+    return tags_data
+
+
+# Function are collecting tags from CSV_VALIDATION_TABLE with validation data to Dict
+def get_validation_folder_classes():
+    tags_data = {}
+
+    response = requests.get(CSV_VALIDATION_TABLE)
+    assert response.status_code == 200, 'Wrong status code'
+
+    csv_file = io.StringIO(response.text, newline='\r\n')
+    reader = csv.reader(csv_file, delimiter=',')
+    next(reader)
+
+    for row in reader:
+        tags_data[row[0]]: Dict[int] = list(map(int, row[1:]))
 
     return tags_data
 
@@ -61,8 +79,30 @@ def get_renders_data():
     return result
 
 
-data = get_renders_data()
+# Function are collecting information about validation images to List
+# where each item have tags and absolute path to image like this
+# [ [[0,1,0,0,1], 'path\to\image.jpg'], [], ... ]
+def get_validation_data():
+    result = []
 
+    # open csv file with tags and save in dict
+    tags_data = get_validation_folder_classes()
+    images_dir = os.path.join(CURRENT_DIR, '..', 'validation')
+
+    for tag in tags_data:
+        result.append([
+            tags_data[tag],
+            os.path.join(images_dir, tag + '.jpg'),
+        ])
+
+    return result
+
+
+# ===========================================
+# --- TRAIN DATA ----------------------------
+# -------------------------------------------
+
+data = get_renders_data()
 
 for slice_index in range(DATA_SLICE):
     print('start work slice with', slice_index, 'of', DATA_SLICE)
@@ -93,8 +133,8 @@ for slice_index in range(DATA_SLICE):
     test_label = np.array(test_label_list).astype('uint8')
 
     slice_str = str(slice_index).zfill(3)
-    train_file = os.path.join(CURRENT_DIR, '..', 'data/train' + slice_str + '.h5')
-    test_file = os.path.join(CURRENT_DIR, '..', 'data/test' + slice_str + '.h5')
+    train_file = os.path.join(CURRENT_DIR, '..', 'data', 'train' + slice_str + '.h5')
+    test_file = os.path.join(CURRENT_DIR, '..', 'data', 'test' + slice_str + '.h5')
 
     with h5py.File(train_file, 'w') as hf:
         hf.create_dataset('train_data', data=train_data)
@@ -103,3 +143,26 @@ for slice_index in range(DATA_SLICE):
     with h5py.File(test_file, 'w') as hf:
         hf.create_dataset('test_data', data=test_data)
         hf.create_dataset('test_label', data=test_label)
+
+
+# ===========================================
+# --- VALIDATION DATA -----------------------
+# -------------------------------------------
+
+validation_data = get_validation_data()
+validation_data_list = []
+validation_label_list = []
+
+for validation in validation_data:
+    image = img_to_array(load_img(validation[1], grayscale=True))
+
+    validation_data_list.append(image)
+    validation_label_list.append(validation[0])
+
+validation_data = np.array(validation_data_list).astype('uint8')
+validation_label = np.array(validation_label_list).astype('uint8')
+
+validation_file = os.path.join(CURRENT_DIR, '..', 'data', 'validation.h5')
+with h5py.File(validation_file, 'w') as hf:
+    hf.create_dataset('data', data=validation_data)
+    hf.create_dataset('label', data=validation_label)
